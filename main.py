ad07dabe2e80254caa0e3a3f22289e13b01e995f
Alexis Merlo <kylian.alexmerlo@gmail.com>
2025-08-13 14:01:47 +0200

    syntaxe

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 from Menu import afficher_menu, rec_reg, roles, inscription, roles_possibles
-import os 
+import os
+
 def main():
+    
     print(f"Suppression de la sauvegarde précédente...")
     fichier_json = "Joueurs.json"
+
     with open("Joueurs.json", "w") as f:
         f.write("[]") 
         print(f'Sauvegarde précédente supprimée..!')
         os.system('cls' if os.name == 'nt' else 'clear')
+
     while True:
         choix = afficher_menu()
         print(f"DEBUG: choix = '{choix}'")
+        
         if choix == "0":
             print("Programme terminé.")
             break
+        
         elif choix == "1":
             inscription(fichier_json, roles_possibles)
+        
         elif choix == "2":
             rec_reg("regles.txt")
+        
         elif choix == "3":
             roles("roles.txt")
+        
         elif choix == "4":
             pass
+        
         else:
             print("Choix invalide, veuillez réessayer.")
 
