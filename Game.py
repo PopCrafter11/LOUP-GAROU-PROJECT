4482d3194a2b702e2ee07f03c6923264747a28a4
Alexis Merlo <kylian.alexmerlo@gmail.com>
2025-08-14 17:11:41 +0200

    improvements

--- Game.py
+++ Game.py
@@ -1,1 +1,27 @@
+def demarrer():
+        
+        print("C'est la nuit, tout le village s'endort, " \
+        "les joueurs ferment les yeux." \
+        "Le voleur se réveille !")
+        
+        reponse = input("Avec qui veux tu échanger ta carte : ")
+        
+        print("Le voleur regarde la carte qu'il a volé et se rendort, ensuite," \
+        "la personne qui s'est fait volé sa carte se réveille et regarde son rôle.")
+        
+        print("Cupidon se réveille !")
+        
+        amoureux1 = input("Désigne le premier amoureux :")
+        amoureux2 = input("désigne le second amoureux : ")
 
+        print("Les 2 amoureux se réveillent, se regardent et tombe amoureux" \
+        "l'un pour l'autre, puis ils se rendorment")
+
+        print("La voyante se réveille, et désigne un joueur dont \
+              elle veut sonder la véritable personnalité !")
+        
+        reponse2 = input("De quel joueur souhaites-tu connaître sa véritable identité ? : ")
+
+        print("Voici le rôle de", reponse2, ": ")
+
+        print("les Loups-Garous se réveillent, se reconnaissent et désignent une nouvelle victime !")
--- Menu.py
+++ Menu.py
@@ -120,28 +120,3 @@ def rec_reg(fichier: str):
     
     else:
         print("Il n\'existe pas de règles comportant ce mot")
-
-def demarrer():
-    nb_tours = 1
-    
-    if nb_tours == 1:
-        
-        print("C'est la nuit, tout le village s'endort, " \
-        "les joueurs ferment les yeux." \
-        "Le voleur se réveille !")
-        
-        reponse = input("Avec qui veux tu échanger ta carte : ")
-        
-        print("Le voleur regarde la carte qu'il a volé et se rendort, ensuite," \
-        "la personne qui s'est fait volé sa carte se réveille et regarde son rôle.")
-        
-        print("Cupidon se réveille !")
-        
-        amoureux1 = input("Désigne le premier amoureux :")
-        amoureux2 = input("désigne le second amoureux : ")
-
-        print("Les 2 amoureux se réveillent, se regardent et tombe amoureux" \
-        "l'un pour l'autre, puis ils se rendorment")
-
-        print("La voyante se réveille, et désigne un joueur dont \
-              elle veut sonder la véritable personnalité !")
